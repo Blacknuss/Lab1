56d0170b07427a1cbf8c51449381e6e808351df7
USER <matvei525724@gmail.com>
2026-03-02 02:36:02 +0800

    First version

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Lab1
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Автор: Павлов Матвей Сергеевич");
+            Console.WriteLine("=== Генератор картинок по запросу (Версия 1) ===");
+            Console.WriteLine("Напишите ключевой тэг, чтобы найти и открыть картинку в браузере.");
+            string keyword = "https://loremflickr.com/500/500/" + Console.ReadLine();
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = keyword,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            Console.WriteLine("Нажмите любую клавишу для выхода.");
             Console.ReadKey();
         }
     }
