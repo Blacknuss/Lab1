ac3797875e3d43c26fcbc7271567ea5844dff6e3
USER <USER@DESKTOP-AAB83N1>
2026-03-02 01:18:02 +0800

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Автор: Павлов Матвей Сергеевич");
+            Console.ReadKey();
+        }
+    }
+}
